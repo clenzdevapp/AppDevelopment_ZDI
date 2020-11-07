981a3923d96bae26329e755423166a7f4b4f6a0f
mclenz2018 <41286288+mclenz2018@users.noreply.github.com>
2020-11-07 15:04:53 +0100

    Create Stringbearbeitung.dart

---
+++ Stringbearbeitung.dart
@@ -0,0 +1,15 @@
+void main() {
+
+String meinName = "Timo";
+String Satz ="Hallo mein Name ist $meinName";
+print (Satz);
+
+double Zaehlchen = 2.3 ;
+String Wert = Zaehlchen.toString();
+print (Wert);
+
+int Laenge = Wert.length;
+  
+print("Ich bin $Laenge Zeichen lang und habe folgendes $Zaehlchen");
+
+}
