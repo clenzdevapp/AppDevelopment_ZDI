e2eeebe42b08965de8c3be4c3392e9ba1f20341f
mclenz2018 <41286288+mclenz2018@users.noreply.github.com>
2020-11-07 14:08:14 +0100

    Create 1.dart

---
+++ 1.dart
@@ -0,0 +1,18 @@
+main() {
+
+  int alter = 17;
+  int geburtsjahr = 2003;
+
+  double klasse = 12.1;
+  double groesse = 1.68;
+
+  String name = "Maren";
+  String nachname = "Melder";
+
+  print(alter);
+  print(geburtsjahr);
+  print(klasse);
+  print(groesse);
+  print(name);
+  print(nachname);
+}
